9281aa3aa54190141bfb80559e5b63d215a11f1b
Asias He <asias@cloudius-systems.com>
2014-05-23 15:08:57 +0300

    libc: Add __h_errno_location
    This is needed to run iperf3.
    
    https://github.com/cloudius-systems/osv/issues/305
    
    Signed-off-by: Asias He <asias@cloudius-systems.com>

--- libc/build.mk
+++ libc/build.mk
@@ -376,6 +376,7 @@ libc += network/inet_pton.o
 libc += network/inet_ntop.o
 libc += network/proto.o
 libc += network/if_indextoname.o
+libc += network/h_errno.o
 
 libc += prng/rand.o
 libc += prng/random.o
