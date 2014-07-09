41e9d69a84def3ac5b0959b62b30f15b81f996dd
Asias He <asias@cloudius-systems.com>
2014-07-09 15:04:32 +0800

    fix uma_zalloc_arg uma_zfree_arg
    Signed-off-by: Asias He <asias@cloudius-systems.com>

--- bsd/porting/uma_stub.cc
+++ bsd/porting/uma_stub.cc
@@ -33,7 +33,7 @@ bool uma_zone::cache::free(void* obj)
 void * uma_zalloc_arg(uma_zone_t zone, void *udata, int flags)
 {
 
-    if (zone->uz_nitems >> zone->uz_nitems_max) {
+    if (zone->uz_nitems_max != 0 && zone->uz_nitems >= zone->uz_nitems_max) {
         debug("uz_nitems=%d,uz_nitems_max=%d\n", zone->uz_nitems.load(std::memory_order_relaxed), zone->uz_nitems_max);
         return NULL;
     }
@@ -60,8 +60,6 @@ void * uma_zalloc_arg(uma_zone_t zone, void *udata, int flags)
         } else {
             ptr = malloc(size);
         }
-
-        zone->uz_nitems++;
 
         bzero(ptr, zone->uz_size);
 
@@ -90,6 +88,8 @@ void * uma_zalloc_arg(uma_zone_t zone, void *udata, int flags)
         UMA_ITEM_HDR(zone, ptr)->refcnt = 1;
     }
 
+    zone->uz_nitems++;
+
     return (ptr);
 }
 
@@ -110,6 +110,7 @@ void uma_zfree_arg(uma_zone_t zone, void *item, void *udata)
 
     WITH_LOCK(preempt_lock) {
         if ((*zone->percpu_cache)->free(item)) {
+            zone->uz_nitems--;
             return;
         }
     }
@@ -118,7 +119,6 @@ void uma_zfree_arg(uma_zone_t zone, void *item, void *udata)
         zone->uz_fini(item, zone->uz_size);
     }
 
-    zone->uz_nitems--;
 
     auto effective_size = zone->uz_size;
     if (zone->uz_flags)
@@ -129,6 +129,8 @@ void uma_zfree_arg(uma_zone_t zone, void *item, void *udata)
     } else {
        free(item);
     }
+
+    zone->uz_nitems--;
 }
 
 void uma_zfree(uma_zone_t zone, void *item)
