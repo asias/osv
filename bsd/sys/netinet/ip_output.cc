a979280b920a1231cf6c369480123a90a39aa38a
Asias He <asias@cloudius-systems.com>
2014-07-23 15:06:08 +0800

    net: Remove ifa_ref()/ifa_free(), which are atomic(9), from ip_output()
    This is backport from freebsd:
    
       commit 2c50988dca8f1c498ada4b3c8c0c275a7c324622
       Author: glebius <glebius@FreeBSD.org>
       Date:   Tue Mar 4 19:49:41 2014 +0000
    
           Remove ifa_ref()/ifa_free(), which are atomic(9), from ip_output().
    
           The ifaddr is already referenced by the rtentry, and we are holding
           reference on the rtentry throughout the function execution.
    
           Sponsored by:       Netflix
           Sponsored by:       Nginx, Inc.
    
    Signed-off-by: Asias He <asias@cloudius-systems.com>

--- bsd/sys/netinet/ip_output.cc
+++ bsd/sys/netinet/ip_output.cc
@@ -272,7 +272,6 @@ again:
 			goto bad;
 		}
 		ia = ifatoia(rte->rt_ifa);
-		ifa_ref(&ia->ia_ifa);
 		ifp = rte->rt_ifp;
 		rte->rt_rmx.rmx_pksent++;
 		if (rte->rt_flags & RTF_GATEWAY)
@@ -510,8 +509,6 @@ sendit:
 			error = netisr_queue(NETISR_IP, m);
 			goto done;
 		} else {
-			if (ia != NULL)
-				ifa_free(&ia->ia_ifa);
 			goto again;	/* Redo the routing table lookup. */
 		}
 	}
@@ -543,8 +540,6 @@ sendit:
 		m->m_hdr.mh_flags |= M_SKIP_FIREWALL;
 		m->m_hdr.mh_flags &= ~M_IP_NEXTHOP;
 		m_tag_delete(m, fwd_tag);
-		if (ia != NULL)
-			ifa_free(&ia->ia_ifa);
 		goto again;
 	}
 
@@ -655,8 +650,6 @@ passout:
 done:
 	if (ro == &iproute)
 		RO_RTFREE(ro);
-	if (ia != NULL)
-		ifa_free(&ia->ia_ifa);
 	return (error);
 bad:
 	m_freem(m);
